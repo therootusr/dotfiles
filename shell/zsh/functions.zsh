2e8150e0f425233b8f2405cd510d81844599f89f
ps <24826492+therootusr@users.noreply.github.com>
2026-05-17 16:55:19 +0530

    --shortstat instead of --stat in fzf preview to dedup info

--- shell/zsh/functions.zsh
+++ shell/zsh/functions.zsh
@@ -128,7 +128,7 @@ function gac() {
   local target="${1:-}"
   if [[ -z "$target" ]]; then
     if command -v fzf >/dev/null 2>&1; then
-      target=$(git log --oneline --decorate --no-color -n 30 | fzf --ansi --prompt="commit> " --delimiter=' ' --preview='git show --format=fuller --stat --numstat -p --color=always {1}' | awk '{print $1}')
+      target=$(git log --oneline --decorate --no-color -n 30 | fzf --ansi --prompt="commit> " --delimiter=' ' --preview='git show --format=fuller --numstat --shortstat -p --color=always {1}' | awk '{print $1}')
     else
       git log --oneline --decorate -n 30
       echo -n "Commit: "
@@ -189,7 +189,7 @@ function glz() {
       fzf --ansi \
           --prompt="commit> " \
           --delimiter=' ' \
-          --preview='git show --format=fuller --stat --numstat -p --color=always {1}' | \
+          --preview='git show --format=fuller --numstat --shortstat -p --color=always {1}' | \
       awk '{print $1}'
 }
 
